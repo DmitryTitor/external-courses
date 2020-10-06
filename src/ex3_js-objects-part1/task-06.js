eb30b1009b1ada9c2ef7cb2e53ee5efa19f3b212
Dima <megamitdimas@gmail.com>
2020-10-06 22:17:03 +0300

    fixed#4 task-06 redone!

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,26 +1,19 @@
 function deepCloneObject(obj) {
-  const newObj = {};
-  
-  for (let key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)){
-      let value = obj[key];
+  const newObj = Array.isArray(obj) ? [] : {};
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const value = obj[key];
 
       if (value !== null && typeof value === 'object') {
         newObj[key] = deepCloneObject(value);
-
-        // Для перевода из объекта в массив нужно поле 'length' - длина массива.
-        // Array.from() переводит объект в массив.
-        if (Array.isArray(value)) {
-          newObj[key].length = value.length;
-          Array.from(newObj[key]);
-        }
       } else {
         newObj[key] = value;
       }
     }
-  }  
+  }
 
   return newObj;
-}  
+}
 
 module.exports = deepCloneObject;
