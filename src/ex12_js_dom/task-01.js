cfcbbd640f36f7039609a57c6ea022ff06a03ed6
Dima <megamitdimas@gmail.com>
2020-11-10 22:43:32 +0300

    fixed slider photo

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,44 +1,41 @@
-function createDefaultElement() {
-  const newElement = document.createElement('img');
-  const divSlider = document.querySelector('.slider__pictures');
-
-  newElement.classList.add('img');
-  newElement.setAttribute('src', 'img/img1.jpg');
-  divSlider.appendChild(newElement);
-}
-
-function changeImg(imgArray, currentIndex, isNextImg) {
-
-  const divSlider = document.querySelector('.slider__pictures');
-  const oldImg = document.querySelector('.img');
-  const newImg = document.createElement('img');
-  let index;
-
-  if(isNextImg) {
-    index = currentIndex >= imgArray.length - 1 ? 0 : (currentIndex + 1);
-  } else {
-    index = currentIndex <= 0 ? imgArray.length - 1 : (currentIndex - 1);
-  }
-
-  newImg.classList.add('img');
-  newImg.setAttribute('src', imgArray[index]);
-  divSlider.classList.add('div-animation');
-  newImg.classList.add('img-animation-to');
-  divSlider.replaceChild(newImg, oldImg);
-
-  return index;
-}
-
+const divSlider = document.querySelector('.slider__pictures');
+const newImg = document.querySelector('.img');
 const butBack = document.querySelector('.slider__button_back');
 const butForth = document.querySelector('.slider__button_forth');
 const imgArray = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg', 'img/img5.jpg'];
 let currentIndex = 0;
 
-butBack.addEventListener("click", () => {
-  return (currentIndex = changeImg(imgArray, currentIndex, false))
-}, false);
-butForth.addEventListener("click", () => {
-  return (currentIndex = changeImg(imgArray, currentIndex, true));
-}, false);
+newImg.setAttribute('src', 'img/img1.jpg');
+butBack.addEventListener("click", () => {return (changeImg(false))}, false);
+butForth.addEventListener("click", () => {return (changeImg(true));}, false);
 
-createDefaultElement();
+function changeImg(isNextImg) { 
+  if (newImg.classList.contains('img-animation-to')) {
+    newImg.classList.remove('img-animation-to');
+  } 
+
+  if (newImg.classList.contains('img-animation-to-reverted')) {
+    newImg.classList.remove('img-animation-to-reverted');
+  } 
+
+  if(isNextImg) {
+    currentIndex = currentIndex >= imgArray.length - 1 ? 0 : (currentIndex + 1);
+  } else {
+    currentIndex = currentIndex <= 0 ? imgArray.length - 1 : (currentIndex - 1);
+  }
+
+  setTimeout(() => {
+    butBack.removeAttribute('disabled');
+    butForth.removeAttribute('disabled'); 
+  }, 2000);
+
+  newImg.setAttribute('src', imgArray[currentIndex]);
+  butBack.setAttribute('disabled', 'true');
+  butForth.setAttribute('disabled', 'true');
+
+  if (isNextImg) {
+    newImg.classList.add('img-animation-to');
+  } else {
+    newImg.classList.add('img-animation-to-reverted');
+  }
+}
